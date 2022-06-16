f03fed8a27d16ea29870030b64b3d6af61584b5c
Felix Zhu <fzhu@atlassian.com>
2022-06-16 13:50:53 +1000

    .

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sparksql-helper",
+    version="0.0.2",
+    license="MIT",
+    author="Felix Zhu",
+    author_email="zhu.felix@outlook.com",
+    description="SparkSQL Helper",
+    long_description=long_description,
+    packages=find_packages(),
+    setup_requires=["setuptools_scm"],
+    url="https://github.com/felixzhu17/SparkSqlHelper",
+    install_requires=[
+        "pandas",
+        "loguru",
+        "tqdm",
+        "pyspark",
+    ],
+)
